c7c49f7e7a62629e40e996b42809a1c58df041d1
Bakare Abdussalam <bakareabdussalam7@gmail.com>
2026-06-28 17:12:06 +0100

    Feature/db connection (#3)
    * Added DBConnectionConfig class for centralized DB setup.

---
+++ src/com/github/ehealthcaremanagement/config/DBConnectionConfig.java
@@ -0,0 +1,21 @@
+package com.github.ehealthcaremanagement.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnectionConfig {
+        public class DBConnection {
+            private static final String URL = "jdbc:mysql://localhost:3306/e-healthcare management database";
+            private static final String USER = "root";
+            private static final String PASSWORD = "";
+
+            public static Connection connect() {
+                try {
+                    return DriverManager.getConnection(URL,USER,PASSWORD);
+                } catch (SQLException e) {
+                    System.err.println("Check your URL,USER or PASSWORD");
+                }return connect();
+            }
+        }
+}
