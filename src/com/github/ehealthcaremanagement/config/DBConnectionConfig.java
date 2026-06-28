482228fcb50b998b8938a0bfd51475c7e709d05f
Solouser-9 <bakareabdussalam7@gmail.com>
2026-06-28 09:35:29 +0100

    Add DBConnectionConfig class for centralized DB setup

--- src/com/github/ehealthcaremanagement/config/DBConnectionConfig.java
+++ src/com/github/ehealthcaremanagement/config/DBConnectionConfig.java
@@ -5,17 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnectionConfig {
-    public class DBConnection {
-        private static final String URL = "jdbc:mysql://localhost:3306/e-healthcare management database";
-        private static final String USER = "root";
-        private static final String PASSWORD = "";
+        public class DBConnection {
+            private static final String URL = "jdbc:mysql://localhost:3306/e-healthcare management database";
+            private static final String USER = "root";
+            private static final String PASSWORD = "";
 
-        public static Connection connect() {
-        try {
-            return DriverManager.getConnection(URL,USER,PASSWORD);
-        } catch (SQLException e) {
-            System.err.println("Check your URL,USER or PASSWORD");
-        }return connect();
-    }
-    }
+            public static Connection connect() {
+                try {
+                    return DriverManager.getConnection(URL,USER,PASSWORD);
+                } catch (SQLException e) {
+                    System.err.println("Check your URL,USER or PASSWORD");
+                }return connect();
+            }
+        }
 }
