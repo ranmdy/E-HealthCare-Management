84719b866c26fe794b1e3835c296b9f202cab44f
Solouser-9 <bakareabdussalam7@gmail.com>
2026-06-29 09:43:49 +0100

    Added DBConnectionConfig.java

--- .idea/src/main/java/com/github/ehealthcaremanagement/config/DBConnectionConfig.java
+++ .idea/src/main/java/com/github/ehealthcaremanagement/config/DBConnectionConfig.java
@@ -1,18 +1,4 @@
 package com.github.ehealthcaremanagement.config;
-/*
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public class DBConnectionConfig {
-    private static final String URL = "jdbc:mysql://localhost:3307/e-healthcare management database";
-    private static final String USER = "Abdussalam";       // change per user
-    private static final String PASSWORD = "Abdussalam123";
-
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
-    }
-}*/
 
 import java.io.IOException;
 import java.io.InputStream;
