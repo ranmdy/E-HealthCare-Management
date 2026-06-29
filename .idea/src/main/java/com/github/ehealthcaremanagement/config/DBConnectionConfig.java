011c9c71ea935fe45cc36bca943daad0d11ed9b1
Solouser-9 <bakareabdussalam7@gmail.com>
2026-06-29 09:43:23 +0100

    Added DBConnectionConfig.java

--- .idea/src/main/java/com/github/ehealthcaremanagement/config/DBConnectionConfig.java
+++ .idea/src/main/java/com/github/ehealthcaremanagement/config/DBConnectionConfig.java
@@ -1,5 +1,5 @@
 package com.github.ehealthcaremanagement.config;
-
+/*
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -12,7 +12,47 @@ public class DBConnectionConfig {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
+}*/
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnectionConfig {
+
+    private static String url;
+    private static String user;
+    private static String password;
+
+    static {
+        try (InputStream input = DBConnectionConfig.class
+                .getClassLoader()
+                .getResourceAsStream("dbconfig.properties")) {
+
+            if (input == null) {
+                throw new RuntimeException("Unable to find dbconfig.properties");
+            }
+
+            Properties prop = new Properties();
+            prop.load(input);
+
+            url = prop.getProperty("db.url");
+            user = prop.getProperty("db.user");
+            password = prop.getProperty("db.password");
+
+        } catch (IOException ex) {
+            throw new RuntimeException("Error loading database configuration", ex);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
+
 
 
 
